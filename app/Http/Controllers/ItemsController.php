1dfa7c1f4244ae4e6116d9494a7559ab821cf9f6
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-04-30 23:53:03 +0900

    configure controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 
+use Validator;
+
 class ItemsController extends Controller
 {
+    private $itemElements = ["product_name", "arrival_source", "manufacturer", "email", "tel"];
+    private $validator = [
+        "product_name" => "required",
+        "email" => "required|string",
+        "tel" => "required|integer",
+    ];
+
     public function index()
     {
         // $items = Item::all();
@@ -21,5 +30,26 @@ class ItemsController extends Controller
     }
     public function store(Request $request)
     {
+        $input = $request->only($this->itemElements);
+
+        $validator = Validator::make($input, $this->validator);
+        if ($validator->fails()) {
+            return redirect()->action("ItemsController@create")
+            ->withInput()
+            ->withErrors($validator)
+        }
+
+        $request->session()->put("form_input", $input);
+
+        return redirect()->action("ItemsController@confirm");
+    }
+    public function confirm
+    {
+        $input = $request->session()->get("form_input");
+
+        if(!$input){
+            return redirect()->action("ItemsController@create");
+        }
+        return view("items/confirm", ["input"=>$input]);
     }
 }
