d0ebf185dcb1057f6c24bfab79dbd1f413d7aebd
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-05-05 02:00:51 +0900

    add new file

---
+++ app/Mail/InquiryMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InquiryMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    protected $contact;
+
+    public function __construct($inquiry)
+    {
+        //
+        $this->inquiry = $inquiry;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('test@gmail.com')
+            ->subject('お問い合わせ')
+            ->view('inquiry.mail')
+            ->with(['inquiry' => $this->inquiry]);
+    }
+}
---
+++ resources/views/inquiry/mail.blade.php
@@ -0,0 +1,6 @@
+{{$inquiry["inquirer_name"]}}様
+以下の内容でお問い合わせを承りました。
+
+email:{{$inquiry["email"]}}
+tel:{{$inquiry["tel"]}}
+お問い合わせ内容：{{$inquiry["inquiry_text"]}}
