cc8d6aa026e54327db38ff0bbf6116a0e29c0d1f
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-05-29 04:07:06 +0900

    ajax

--- resources/views/shippings/create.blade.php
+++ resources/views/shippings/create.blade.php
@@ -2,33 +2,21 @@
 
 @section('content')
   <h4>出荷先登録</h4>
-  @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-      </ul>
+    <div id="shipping-error-info" class="alert alert-danger d-none">
     </div>
-    <?php
-      $name = old('name');
-      $address = old('address');
-      $tel = old('tel');
-    ?>  
-  @endif
   <div class="w-50" id="ajax-contents">
       <div class="form-group">
         <label for="product_name">出荷先名(必須)</label>
-        <input id="name" class="form-control" type="text" name="name" value="{{ old('name')}}">
+        <input id="name" class="form-control" type="text" name="name">
       </div>
       <div class="form-group">
         <label for="address">住所(必須)</label>
-        <input id="address" class="form-control" type="text" name="address" value="{{ old('address')}}">
+        <input id="address" class="form-control" type="text" name="address">
       </div>
       <div class="form-group">
         <label for="tel">TEL(必須)
         </label>
-        <input id="tel" class="form-control" type="text" name="manufacturer" value="{{ old('tel')}}">
+        <input id="tel" class="form-control" type="text" name="manufacturer">
       </div>
       <button id="shipping-register" class="btn btn-primary">登録</button>
   </div>
@@ -36,55 +24,112 @@
   <div id="ajax-finished"></div>
   <script>
     window.onload = function(){
-      $("#shipping-register").click(function(event){
-        const shipping_name = $("#name").val();
-        const shipping_address = $("#address").val();
-        const shipping_tel = $("#tel").val();
-        console.log(shipping_name);
-        const confirmHTML = `<div class="form-group">
-                              <label>出荷先名</label>
-                              <div>${shipping_name}</div>
-                            </div>      
-                            <div class="form-group">
-                              <label>住所</label>
-                              <div>${shipping_address}</div>
+      function registerListen() {
+        document.querySelector('#shipping-register').addEventListener("click", function () {
+          $("#shipping-error-info").addClass('d-none');
+          $("#shipping-error-info").empty();
+          const shipping_name = $("#name").val();
+          const shipping_address = $("#address").val();
+          const shipping_tel = $("#tel").val();
+          const inputHTML = `<div class="form-group">
+                              <label for="product_name">出荷先名(必須)</label>
+                              <input id="name" class="form-control" type="text" name="name" value="${shipping_name}">
                             </div>
                             <div class="form-group">
-                              <label>TEL</label>
-                              <div>${shipping_tel}</div>
+                              <label for="address">住所(必須)</label>
+                              <input id="address" class="form-control" type="text" name="address" value="${shipping_address}">
                             </div>
-                            <button id="confirm-back" class="btn btn-secondary">戻る</button>
-                            <button id="confirm-register" class="btn btn-primary">登録</button>
-                          `;
-        $("#ajax-contents").remove();
-        $("#ajax-confirm").append(confirmHTML);
-        document.querySelector('#confirm-register').addEventListener("click", function () {
-          $.ajax({
-            headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            url: "/shipping/add",
-            type: "POST",
-            data: {
-              'name': shipping_name,
-              'address': shipping_address,
-              'tel': shipping_tel,
-            },
+                            <div class="form-group">
+                              <label for="tel">TEL(必須)</label>
+                              <input id="tel" class="form-control" type="text" name="manufacturer" value="${shipping_tel}">
+                            </div>
+                              <button id="shipping-register" class="btn btn-primary">登録</button>
+                            </div>
+                            `;
 
-          }).done(function(data){
-            const finishedHTML = `<h3>登録完了</h3>
-                                  <p>${data}</p>
-                                  <a href="{{url('/items')}}">戻る</a>`;
-            $("#ajax-confirm").remove();
-            $("#ajax-finished").append(finishedHTML);
-          }).fail(function(err){
-            console.log(err);
+          const confirmHTML = `<div class="form-group">
+                                <label>出荷先名</label>
+                                <div>${shipping_name}</div>
+                              </div>      
+                              <div class="form-group">
+                                <label>住所</label>
+                                <div>${shipping_address}</div>
+                              </div>
+                              <div class="form-group">
+                                <label>TEL</label>
+                                <div>${shipping_tel}</div>
+                              </div>
+                              <button id="confirm-back" class="btn btn-secondary">戻る</button>
+                              <button id="confirm-register" class="btn btn-primary">登録</button>
+                            `;
+          $("#ajax-contents").empty();
+          $("#ajax-confirm").append(confirmHTML);
+          window.history.pushState(null, null, '/shipping/confirm');
+          window.addEventListener('popstate', function(event){
+            // event.preventDefault();
+            $("#ajax-confirm").empty();
+            $("#ajax-contents").empty();
+            $("#ajax-contents").append(inputHTML);
+            window.history.replaceState(null, null,'/shipping/create');
+            registerListen();
           });
-          ;
+          document.querySelector('#confirm-back').addEventListener("click", function () {
+            $("#ajax-confirm").empty();
+            $("#ajax-contents").append(inputHTML);
+            window.history.replaceState(null, null,'/shipping/create');
+            registerListen();
+          });
+          document.querySelector('#confirm-register').addEventListener("click", function () {
+            $.ajax({
+              headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+              },
+              url: "/shipping/add",
+              type: "POST",
+              data: {
+                'name': shipping_name,
+                'address': shipping_address,
+                'tel': shipping_tel,
+              },
+
+            }).done(function(data){
+              const finishedHTML = `<h3>登録完了</h3>
+                                    <p>${data}</p>
+                                    <a href="{{url('/items')}}">商品一覧に戻る</a>`;
+              $("#ajax-confirm").remove();
+              $("#ajax-finished").append(finishedHTML);
+              window.history.replaceState(null, null,'/shipping/complete');
+              window.addEventListener('popstate', function(event){
+                $("#ajax-finished").empty();
+                $("#ajax-contents").empty();
+                $("#ajax-contents").append(inputHTML);
+                window.history.replaceState(null, null,'/shipping/create');
+                registerListen();
+              });
+
+            }).fail(function(err){
+              const responseError = JSON.parse(err.responseText).errors;
+              let errorList = '';
+              Object.keys(responseError).forEach(key => {
+                errorList = `${errorList} <li>${responseError[key]}</li>`;
+              });
+              const errorHTML = `<ul>${errorList}</ul>`;
+              $("#shipping-error-info").append(errorHTML);
+              $("#shipping-error-info").removeClass('d-none');
+              $("#ajax-confirm").empty();
+              $("#ajax-contents").append(inputHTML);
+              window.history.replaceState(null, null,'/shipping/create');
+              registerListen();
+              
+            });
+
+          });
 
         });
+      }
 
-      });
+      registerListen();
+
 
 
     };
