d187185f67047c77d32854b2ad497522635071d7
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-06-07 00:56:08 +0900

    create view

---
+++ resources/views/inquiry/csv.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+  @section('content')
+    <h4>お問合せ内容のダウンロード</h4>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+
+  @endif
+  <div class="w-50">
+    <form method="post" action="{{ url('/inquiry/csv') }}">
+      @csrf
+      <div class="form-group">
+        <label for="inquirer_name">お問い合わせ（年）(必須)</label>
+        <input id="inquirer_name" class="form-control" type="text" name="inquiry_year">
+      </div>
+      <div class="form-group">
+        <label for="email">お問い合わせ（月）(必須)</label>
+        <input id="email" class="form-control" type="text" name="inquiry_month">
+      </div>
+      <button type="submit" class="btn btn-primary">ダウンロード</button>
+    </form>
+  </div>
+
+
+@endsection
