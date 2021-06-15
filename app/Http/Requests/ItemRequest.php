7ef86bff313af0fbe7eefce71d80f6982776d564
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-06-16 00:55:27 +0900

    createItemRequest

---
+++ app/Http/Requests/ItemRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Request;
+
+class ItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            "product_name" => "required|string",
+            "arrival_source" => "nullable|string",
+            "manufacturer" => "nullable|string",
+            "email" => "required|string|email:strict,dns",
+            "tel" => "required|regex:/^[0-9\-]+$/i",
+
+
+        ];
+    }
+}
